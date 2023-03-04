5f4b80908300262e3edff638468a05edfe5a699e
umatt1 <matt.burkhard@gmail.com>
2023-03-04 16:22:01 -0500

    Changes how service account credentials passed in

--- pages/api/create-room.ts
+++ pages/api/create-room.ts
@@ -43,7 +43,7 @@ try {
       auth_uri: "https://accounts.google.com/o/oauth2/auth",
       token_uri: "https://oauth2.googleapis.com/token",
       auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-      client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-e6e6u%40class-connect-f7b87.iam.gserviceaccount.com"
+      client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_CERT_URL
     } as any),
     databaseURL: "https://class-connect-f7b87-default-rtdb.firebaseio.com"
   });
