1e526a5c6ea5f794e6210244af6600ee7deaca5d
Isaiah Kahler <isaiahwkahler@gmail.com>
2023-03-10 11:43:12 -0500

    fix vercel environment variable

--- pages/api/create-room.ts
+++ pages/api/create-room.ts
@@ -30,7 +30,9 @@ try {
       type: "service_account",
       project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+      private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY
+        ? process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/gm, "\n")
+        : undefined,
       client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
       client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
       auth_uri: "https://accounts.google.com/o/oauth2/auth",
@@ -50,7 +52,7 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
 
-  if(!app) {
+  if (!app) {
     res.status(500);
     return;
   }
