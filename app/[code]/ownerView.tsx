f100718f7e099514157c03570ef522d557ca0c18
Isaiah Kahler <isaiahwkahler@gmail.com>
2023-04-12 16:02:10 -0400

    WIP added user management modal

--- app/[code]/ownerView.tsx
+++ app/[code]/ownerView.tsx
@@ -9,7 +9,7 @@ import { getColor } from "@/components/ui/colors";
 import { useState } from "react";
 import styles from './ownerView.module.css';
 import Icon from "@mdi/react";
-import { mdiAccountGroup, mdiNotificationClearAll } from "@mdi/js";
+import { mdiAccountGroup, mdiClose, mdiNotificationClearAll } from "@mdi/js";
 import twemoji from "twemoji";
 import Image from "next/image";
 import ReactionsDisplay from "./components/reactions";
@@ -28,6 +28,8 @@ export default function OwnerView(props: OwnerViewProps) {
   const database = getDatabase(app);
 
   const [confirmDismiss, setConfirmDismiss] = useState(false);
+  const [kickConfirm, setKickConfirm] = useState<string | null>();
+  const [openModal, setOpenModal] = useState(false);
 
 
   if (!room.started) {
@@ -45,6 +47,24 @@ export default function OwnerView(props: OwnerViewProps) {
       setConfirmDismiss(false);
     }
   }
+
+
+  const handleKickParticipant = (id: string) => {
+    if (!kickConfirm) {
+      setKickConfirm(id);
+      setTimeout(() => setKickConfirm(null), 4000);
+    } else if (kickConfirm === id) {
+      // kick player
+      remove(ref(database, `rooms/${code}/participants/${id}`));
+      console.log(1)
+      setKickConfirm(null);
+    } else {
+      // clicked another name
+      console.log(2)
+      setKickConfirm(null);
+    }
+
+  };
 
   return (
     <>
@@ -67,19 +87,67 @@ export default function OwnerView(props: OwnerViewProps) {
         alignItems: 'flex-start'
       }}>
         {/* indicator for number of participants */}
-        <IconButton style={{ padding: '0.25em 0.5em' }}>
+        <IconButton style={{ padding: '0.25em 0.5em' }} onClick={() => setOpenModal(true)}>
           <h2 style={{ display: 'inline', marginRight: '0.25em' }}>{participants ? Object.keys(participants).length : '0'}</h2><Icon path={mdiAccountGroup} size={1.5} />
         </IconButton>
         {/* button to clear all participants */}
         {/* <span style={{display: 'flex'}}> */}
-          <IconButton onClick={dismissAll}>
-            <Icon path={mdiNotificationClearAll} size={1.5} />
-            {confirmDismiss && <p style={{fontWeight: 'bold', paddingRight: '0.5em'}} className={styles.fadeOut}>Click again to dismiss all hands</p>}
-          </IconButton>
+        <IconButton onClick={dismissAll}>
+          <Icon path={mdiNotificationClearAll} size={1.5} />
+          {confirmDismiss && <p style={{ fontWeight: 'bold', paddingRight: '0.5em' }} className={styles.fadeOut}>Click again to dismiss all hands</p>}
+        </IconButton>
         {/* </span> */}
       </span>
 
       {reactions && <ReactionsDisplay reactions={reactions} />}
+
+
+      {openModal && <div style={{
+        position: 'fixed',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 1000,
+        top: 0,
+        backgroundColor: "rgba(0,0,0,0.25)"
+      }}
+        onClick={(e) => {
+          if(e.target !== e.currentTarget) return;
+          console.log('clicked outside')
+        }}
+      >
+        <div style={{
+          width: 'min(680px, 90vw)',
+          height: '80vh',
+          backgroundColor: "#fff",
+          borderRadius: '20px',
+          padding: '1rem 1.5rem',
+          position: 'relative'
+        }}>
+          <h1>participants</h1>
+
+          <IconButton style={{position: 'absolute', top: '1.5em', right: '1.5em', backgroundColor: "transparent", border: '1px solid #000'}} onClick={() => setOpenModal(false)}>
+            <Icon path={mdiClose} size={1} color='#000' />
+          </IconButton>
+
+          <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'wrap'
+          }}>
+            {participants && Object.entries(participants).map(([id, name], index) =>
+              <span key={id} style={{ margin: '0 .5em' }}>
+                <Button onClick={() => handleKickParticipant(id)} style={{ backgroundColor: getColor(index) }}>
+                  <h3 className={kickConfirm === id ? styles.fadeOut : ''}>{kickConfirm === id ? 'click again to kick' : name}</h3>
+                </Button>
+              </span>
+            )}
+
+          </div>
+        </div>
+      </div>}
     </>
   );
 }
